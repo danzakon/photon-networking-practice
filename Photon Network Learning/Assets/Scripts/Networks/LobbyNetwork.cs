9a4720b2e6b4a3eee18248e519f19bc567491c1d
Dan Zakon <danzakon@gmail.com>
2018-07-01 16:30:46 -0400

    Initial commit

---
+++ Photon Network Learning/Assets/Scripts/Networks/LobbyNetwork.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyNetwork : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Connecting to server...");
+		PhotonNetwork.ConnectUsingSettings ("1.0");
+
+	}
+
+	private void OnConnectedToMaster(){
+		print ("Connected to master");
+		PhotonNetwork.playerName = PlayerNetwork.Instance.PlayerName;
+		PhotonNetwork.JoinLobby (TypedLobby.Default);
+	}
+
+	private void OnJoinLobby(){
+		print ("Joined lobby");
+	}
+
+}
---
+++ Photon Network Learning/Assets/Scripts/Networks/PlayerNetwork.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerNetwork : MonoBehaviour {
+
+	public static PlayerNetwork Instance;
+	public string PlayerName { get; private set; }
+
+	// Use this for initialization
+	void Awake () {
+		Instance = this;
+
+		PlayerName = "Dan#" + Random.Range (1000, 9999);
+	}
+
+}
